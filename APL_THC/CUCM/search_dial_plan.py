efae77ad7555fea32c3f1589c06d040548ed4859
Sandeep Kumar Rai <srai2@cisco.com>
2015-04-16 10:47:46 +0530

    Updated Search pattern

--- APL_THC/CUCM/search_dial_plan.py
+++ APL_THC/CUCM/search_dial_plan.py
@@ -1,11 +1,14 @@
-__author__ = 'srai2'
 import re
 
 def split_initial(route):
-    if route[0]=="\\":
-        route = route[2:]
-    if route[0]==".":
-        route=route[1:]
+    route = route.replace("\+[14]+","")
+    route = route.replace("\+44","")
+    route = route.replace("\+1","")
+    route = route.replace("\+.[14]+","")
+    route = route.replace("\+.1","")
+    route = route.replace("\+.44","")
+    route = route.replace("\+","")
+
     if route[0]=="1":
         route=route[1:]
     if route[:2] == "44":
@@ -17,14 +20,8 @@ def Pattern(routes):
     for route in routes:
         pattern = route
         route = split_initial(route)
-        if route[:2]== "44":
-            route = route[2:]
-        elif route[0]=="9" and route[1] != "0" and route[1] != "1":
-            route = route[1:]
         dial_patterns.append((route, pattern))
     return dial_patterns
-
-
 
 def find(s, ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
@@ -44,8 +41,6 @@ def get_pattern_with_proper_length(pattern, length):
             value = value+1
     return pattern[:value]
 
-
-
 def search_dial_plan(string, pattern):
     dial_list = []
     string_length= len(string)
@@ -56,8 +51,9 @@ def search_dial_plan(string, pattern):
        else:
            regex = re.compile(dial_pattern)
            if regex.match(string):
-               print route[1]
                dial_list.append(route[1])
     return list(set(dial_list))
 
+#Testing Scripts
+#search_dial_plan("200", ["\+1[2-5][02468]0XX[79]XXXX", "\+[14]+2[0-1][0-9]XXXXXXX", "\+55577+", "\+[14]+5[4-5][0-9]XXXXXXX", "\+1[2-5][02468]0XX[79]XXXX", "\+44[2-5][02468]0X06XXXX"])
 
